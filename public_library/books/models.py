150e3fd95250e61bda5cfe9470911c9fb28915d4
deKupini <k.kupinski92@gmail.com>
2024-08-24 10:42:43 +0200

    Add book model

--- public_library/books/models.py
+++ public_library/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    borrowed = models.BooleanField(default=False)
+    borrow_date = models.DateField(null=True, default=None)
+    borrower = models.IntegerField(null=True, default=None)
+
+    def __str__(self):
+        return self.title
