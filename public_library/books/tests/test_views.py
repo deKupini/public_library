5d6da8f3946d633292dcb81ba6dd8e77bd09be8a
deKupini <k.kupinski92@gmail.com>
2024-08-25 17:31:49 +0200

    Add borrow book endpoint

--- public_library/books/tests/test_views.py
+++ public_library/books/tests/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+from freezegun import freeze_time
 from rest_framework.test import APIClient
 
 from ..models import Book
@@ -95,3 +96,26 @@ def test_delete_non_existing_book(db):
     response = client.delete('/books/123456/')
     assert response.status_code == 404
     assert not Book.objects.filter(id='123456').exists()
+
+
+@freeze_time("2022-01-01")
+def test_borrow_book(book_2, db):
+    client.patch('/books/789012/borrow/', {'borrower': '123456'})
+    book = Book.objects.get(id='789012')
+    assert book.borrowed
+    assert book.borrow_date.strftime('%Y-%m-%d') == '2022-01-01'
+    assert book.borrower == '123456'
+
+
+def test_borrow_non_existing_book(db):
+    response = client.patch('/books/123456/borrow/', {'borrower': '123456'})
+    assert response.status_code == 404
+
+
+def test_borrow_already_borrowed_book(book_1, db):
+    response = client.patch('/books/123456/borrow/', {'borrower': '123456'})
+    assert response.status_code == 400
+    book = Book.objects.get(id='123456')
+    assert book.borrowed
+    assert book.borrow_date.strftime('%Y-%m-%d') == book_1.borrow_date
+    assert book.borrower == book_1.borrower
--- public_library/books/views.py
+++ public_library/books/views.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Book
-from .serializers import BookSerializer, CreateBookSerializer
+from .serializers import BookSerializer, CreateBookSerializer, PartialUpdateBookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -9,4 +14,17 @@ class BookViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'create':
             return CreateBookSerializer
+        if self.action == 'partial_update':
+            return PartialUpdateBookSerializer
         return BookSerializer
+
+    @action(detail=True, methods=['patch'])
+    def borrow(self, request, pk):
+        book = self.get_object()
+        if book.borrowed:
+            return Response({'message': 'Book already borrowed'}, status=400)
+        book.borrower = request.data['borrower']
+        book.borrow_date = datetime.now().date()
+        book.borrowed = True
+        book.save()
+        return Response({'message': 'Book borrowed successfully'}, status=200)
